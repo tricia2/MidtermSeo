700cf4393ae0668482ee1279a2c99e76682d6c97
Criscia Dama <Criscia Dama>
2017-09-18 18:20:12 +0800

    committed

--- server.js
+++ server.js
@@ -14,24 +14,28 @@ app.get('/', function(req, res){
 app.get('/about', function(req, res){
   res.sendFile(path.join(__dirname, 'views/about.html'));
  });
-app.get('/codes', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/codes.html'));
+app.get('/about2', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/about2.html'));
  });
-app.get('/contact', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/contact.html'));
+app.get('/about3', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/about3.html'));
  });
-app.get('/gallery', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/gallery.html'));
+app.get('/about4', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/about4.html'));
  });
-app.get('/icons', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/icons.html'));
+app.get('/about5', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/about5.html'));
  });
-app.get('/single', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/single.html'));
+app.get('/about6', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/about6.html'));
  });
-app.get('/codes2', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/codes2.html'));
+app.get('/about7', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/about7.html'));
  });
+app.get('/about8', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/about8.html'));
+ });
+
 //express server listen
 var server = app.listen(app.get('port'), function(){
   console.log('Server listening on port 5000 ',app.get('port'));
